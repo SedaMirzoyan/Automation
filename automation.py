521d8481ec6ca953a4d8c2ec346e87ebd8ce7db6
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-06-28 00:16:42 +0400

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import csv
+
+
+directories = glob.glob("*/*/compout/views/*")
+
+
+for d in directories:
+     #directory case
+	if os.path.isdir(d):
+		try:
+			for file_name in os.listdir(d):
+				full_path = os.path.join(d, file_name)
+ 
+				if os.path.isfile(full_path):
+					print(f"{file_name}")
+					if (file_name.endswith(".log")):
+						print(f"'Errors:' in {file_name}")
+						try:
+							with open(full_path, 'r') as log_file:
+								for line in log_file:
+									if "Errors:" in line:
+										ls = line.strip()
+										print(ls)
+						except Exception as e:
+							print(f"Issue with reading file: {e}")
+		except Exception as e:
+			print(f"Issue with reading directory: {e}")
+ 
+	#file case
+	elif os.path.isfile(d) and d.endswith(".log"):
+		print(f"'Errors:' in {d}")
+		try:
+			with open(d, 'r') as log_file:
+				for line in log_file:
+					if "Errors:" in line:
+						ls = line.strip()
+						print(ls)
+		except Exception as e:
+			print(f"Issue with reading file: {e}")
+ 
+
+
