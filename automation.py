99aac1b3458249c2730074b3a160315cb00b1c3b
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-07-06 23:25:46 +0400

    Add files via upload

--- automation.py
+++ automation.py
@@ -4,7 +4,6 @@ import glob
 import os
 import csv
 import re
-import pandas as pd
 
 directories = glob.glob("*/*/compout/views/*")
 
@@ -18,54 +17,60 @@ data = []
 
 
 def findInstances():	
-	with open(txt_file, 'w') as txt:
-		for d in directories:
-			if os.path.isdir(d):
-				print("for testing ", d)
-				try:
+	try:
+		with open(txt_file, 'w') as txt:
+			for d in directories:
+				if os.path.isdir(d):
+					print("for testing ", d)
 					for file_name in os.listdir(d):
 						full_path = os.path.join(d, file_name)
 						if os.path.isfile(full_path) and file_name.endswith(".log"):
-							with open(full_path, 'r') as log_file:
-								for line in log_file:
-									if "Errors:" in line:
-										ls = line.strip()
-										print(ls)
-										instance = os.path.basename(full_path)
-                                        #txt.write(f"{instance} : {line.strip()}\n")
-										txt.write(f"{instance} : {full_path} : {line.strip()}\n")
-				except Exception as e:
-					print(f"Issue with writing in file: {e}")
-			elif os.path.isfile(d):
-				print(f"{d} is file")
+							try:
+								with open(full_path, 'r') as log_file:
+									for line in log_file:
+										if "Errors:" in line:
+											ls = line.strip()
+											print(ls)
+											instance = os.path.basename(full_path)
+                                        	#txt.write(f"{instance} : {line.strip()}\n")
+											txt.write(f"{instance} : {full_path} : {line.strip()}\n")
+							except Exception as re:
+								print(f"Issue with reading file: {re}")
+						elif os.path.isfile(d):
+							print(f"{d} is file")
+	except Exception as we:
+		print(f"Issue with writing in file: {we}")
 
 
 
 
 def writeIntoCsv():
-	with open(txt_file, 'r') as txt:
-		for line in txt:
-			if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
-				print("Failed instance is {")
-				if(len(parts) >= 2):
-					#inst_name = parts[0].strip()
-					inst_name = parts[0]
-					inst_path = parts[1]
-					
-					try:
-						with open(path, 'r') as log_file:
-							for file_line in log_file:
-								if ("ERROR" in file_line):
-									data.append([inst_name, inst_path])
-					
-					except Exception as e:
-						print(f"Issue with reading log file: {e}")	
+	try:
+		with open(txt_file, 'r') as log_file:
+
+			for line in log_file:
+				parts = line.split(":")
+				if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
+					print("Failed instance is {")
+					if(len(parts) >= 2):
+						#inst_name = parts[0].strip()
+						inst_name = parts[0]
+						inst_path = parts[1]
+						try:
+							with open(output_csv, 'w') as csv_out:
+								writer = csv.writer(csv_out)
+								writer.writerow(["Instance name", "Error message"])
+								for file_line in log_file:
+									if ("ERROR" in file_line):
+										writer.writerow([inst_name, file_line.strip()])
+						except Exception as e:
+							print(f"Issue with writing in csv file: {e}")	
+	except Exception as ie:
+		print(f"Issue with reading log file: {ie}")
 
 
-	df = pd.DataFrame(data, columns = ["Instance name", "Error message"])
-	df.to_csv("errors_report_pandas.csv", index = False)								
-			
-			
+
+
 
 
 
