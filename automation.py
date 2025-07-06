8d5eced165c7eeb8f72063290836921117838e09
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-07-06 21:02:31 +0400

    Add files via upload

--- automation.py
+++ automation.py
@@ -3,44 +3,68 @@
 import glob
 import os
 import csv
-
+import re
 
 directories = glob.glob("*/*/compout/views/*")
 
+output_csv = "errors_report.csv"
+txt_file = "errors.txt"
+#search_pattern = re.search(r"\bErrors:\s*(?!0\b)\d+\b")
 
-for d in directories:
-     #directory case
-	if os.path.isdir(d):
-		try:
-			for file_name in os.listdir(d):
-				full_path = os.path.join(d, file_name)
- 
-				if os.path.isfile(full_path):
-					print(f"{file_name}")
-					if (file_name.endswith(".log")):
-						print(f"'Errors:' in {file_name}")
-						try:
+
+
+def findInstances():	
+	with open(txt_file, 'w') as txt:
+		for d in directories:
+			if os.path.isdir(d):
+				print("for testing ", d)
+				try:
+					for file_name in os.listdir(d):
+						full_path = os.path.join(d, file_name)
+						if os.path.isfile(full_path) and file_name.endswith(".log"):
 							with open(full_path, 'r') as log_file:
 								for line in log_file:
 									if "Errors:" in line:
 										ls = line.strip()
 										print(ls)
-						except Exception as e:
-							print(f"Issue with reading file: {e}")
-		except Exception as e:
-			print(f"Issue with reading directory: {e}")
- 
-	#file case
-	elif os.path.isfile(d):
-		print(f"'{d} is file")
-		#try:
-		#	with open(d, 'r') as log_file:
-		#		for line in log_file:
-		#			if "Errors:" in line:
-		#				ls = line.strip()
-		#				print(ls)
-		#except Exception as e:
-		#	print(f"Issue with reading file: {e}")
- 
+										instance = os.path.basename(full_path)
+                                        #txt.write(f"{instance} : {line.strip()}\n")
+										txt.write(f"{instance} : {full_path} : {line.strip()}\n")
+				except Exception as e:
+					print(f"Issue with reading file: {e}")
+			elif os.path.isfile(d):
+				print(f"{d} is file")
 
+
+    
+def writeIntoCsv():
+	with open(txt_file, 'r') as txt, open(output_csv, 'w') as csv_out:
+		writer = csv.writer(csv_out)
+		writer.writerow(["Instance name", "Log File Path"])
+		for line in txt:
+			#print(type(line))
+
+			if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
+				print("Failed instance is {")
+				pattern_match = re.search(r"^(.*?)\s*:\s*", line)
+				#pattern_second_part = re.search(r"\s*:\s*(.*?)\s*:\s*", line)
+				#pattern_second_part = re.search(r":\s*(.*?)\s*:", line)
+				if (pattern_match):
+					result = pattern_match.group(1).strip()
+					writer.writerow([result])				
+			else:
+				print("No failed instance")
+		
+	    
+	    
+    
+findInstances()
+writeIntoCsv()
+
+
+	    
+	    
+	   
+	   
+ 
 
