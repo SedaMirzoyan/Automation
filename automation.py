46b6369ad7b7a466db4b85218a2fdaf14b459214
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-07-06 22:55:34 +0400

    Add files via upload

--- automation.py
+++ automation.py
@@ -4,12 +4,16 @@ import glob
 import os
 import csv
 import re
+import pandas as pd
 
 directories = glob.glob("*/*/compout/views/*")
 
 output_csv = "errors_report.csv"
 txt_file = "errors.txt"
+failed_insts = "failed_insts.txt"
 #search_pattern = re.search(r"\bErrors:\s*(?!0\b)\d+\b")
+
+data = []
 
 
 
@@ -31,30 +35,40 @@ def findInstances():
                                         #txt.write(f"{instance} : {line.strip()}\n")
 										txt.write(f"{instance} : {full_path} : {line.strip()}\n")
 				except Exception as e:
-					print(f"Issue with reading file: {e}")
+					print(f"Issue with writing in file: {e}")
 			elif os.path.isfile(d):
 				print(f"{d} is file")
 
 
-    
-def writeIntoCsv():
-	with open(txt_file, 'r') as txt, open(output_csv, 'w') as csv_out:
-		writer = csv.writer(csv_out)
-		writer.writerow(["Instance name", "Log File Path"])
-		for line in txt:
-			#print(type(line))
 
+
+def writeIntoCsv():
+	with open(txt_file, 'r') as txt:
+		for line in txt:
 			if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
 				print("Failed instance is {")
-				pattern_match = re.search(r"^(.*?)\s*:\s*", line)
-				#pattern_second_part = re.search(r"\s*:\s*(.*?)\s*:\s*", line)
-				#pattern_second_part = re.search(r":\s*(.*?)\s*:", line)
-				if (pattern_match):
-					result = pattern_match.group(1).strip()
-					writer.writerow([result])				
-			else:
-				print("No failed instance")
-		
+				if(len(parts) >= 2):
+					#inst_name = parts[0].strip()
+					inst_name = parts[0]
+					inst_path = parts[1]
+					
+					try:
+						with open(path, 'r') as log_file:
+							for file_line in log_file:
+								if ("ERROR" in file_line):
+									data.append([inst_name, inst_path])
+					
+					except Exception as e:
+						print(f"Issue with reading log file: {e}")	
+
+
+	df = pd.DataFrame(data, columns = ["Instance name", "Error message"])
+	df.to_csv("errors_report_pandas.csv", index = False)								
+			
+			
+
+
+
 	    
 	    
     
