a1bac8fd8ca469f54bf2d415a54cc24a649bb41d
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-07-08 02:46:16 +0400

    Add files via upload

--- automation.py
+++ automation.py
@@ -5,143 +5,184 @@ import os
 import csv
 import re
 from collections import defaultdict
+from abc import ABC, abstractmethod
 
 
-qa_check_path = "/remote/us01sgnfs00693/ss8/ss8-6428856/work/mirzoyan/qa/all"
-const_path = "/*/*/compout/views/*"
 
-full_path_pattern = qa_check_path + const_path
-directories = glob.glob(full_path_pattern)
-
-
-txt_file = "errors.txt"
-output_csv = "errors_report.csv"
-output_html = "errors_report.html"
-
-data = defaultdict(list)
+def makeDirectoryPath(qa_check_path):
+	const_path = "/*/*/compout/views/*"
+	full_path_pattern = qa_check_path + const_path
+	directories = glob.glob(full_path_pattern)	
+	
+	return directories
 
 
-html = [
-    "<html><head><style>",
-    "table { border-collapse: collapse; width: 100%; }",
-    "th, td { border: 1px solid #ccc; padding: 8px; text-align: left; }",
-    "th { background-color: #f2f2f2; }",
-    "</style></head><body>",
-    "<h2>Hello Messages</h2>",
-    "<table>",
-    "<tr><th>Instance name</th><th>Error message</th><th>Log Path</th></tr>"
-]
+
+class RecordingStrategy(ABC):
+    """
+    Abstract base class with recording strategies
+    """
+    @abstractmethod
+    def record(self, qa_check_path):
+        pass
 
 
-def findInstances():	
-	try:
-		with open(txt_file, 'w') as txt:
-			for d in directories:
-				if os.path.isdir(d):
-					print("for testing ", d)
-					for file_name in os.listdir(d):
-						full_path = os.path.join(d, file_name)
-						if os.path.isfile(full_path) and file_name.endswith(".log"):
+
+class RecordingInCsvStrategy(RecordingStrategy):
+
+	def record(self, qa_check_path):
+		output_csv = "errors_report.csv"
+		try:
+			with open(txt_file, 'r') as log_file, open(output_csv, 'w') as csv_out:
+				writer = csv.writer(csv_out)
+				writer.writerow(["Instance name", "Error message", "Log path"])
+				for line in log_file:
+					parts = line.strip().split(":")
+					if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
+						if(len(parts) >= 2):
+							inst_name = parts[0].strip()
+							inst_name = os.path.splitext(inst_name)[0]
+							inst_path = parts[1].strip()
 							try:
-								with open(full_path, 'r') as log_file:
-									for line in log_file:
-										if "Errors:" in line:
-											ls = line.strip()
-											print(ls)
-											instance = os.path.basename(full_path)
-                                        	#txt.write(f"{instance} : {line.strip()}\n")
-											txt.write(f"{instance} : {full_path} : {line.strip()}\n")
-							except Exception as re:
-								print(f"Issue with reading a file: {re}")
-						elif os.path.isfile(d):
-							print(f"{d} is file")
-	except Exception as we:
-		print(f"Issue with writing into a file: {we}")
+								with open(inst_path, 'r') as input_file:
+									for file_line in input_file:
+										if ("ERROR" in file_line):
+											writer.writerow([inst_name, file_line.strip(), inst_path])
+							except Exception as e:
+								print(f"Issue with writing in csv file: {e}")	
+		except Exception as ie:
+			print(f"Issue with reading a log file: {ie}")
 
 
 
 
-def writeIntoCsv():
-	try:
-		with open(txt_file, 'r') as log_file, open(output_csv, 'w') as csv_out:
-			writer = csv.writer(csv_out)
-			writer.writerow(["Instance name", "Error message", "Log path"])
-			for line in log_file:
-				parts = line.strip().split(":")
-				if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
-					print("Failed instance is {")
-					if(len(parts) >= 2):
-						inst_name = parts[0].strip()
-						inst_name = os.path.splitext(inst_name)[0]
-						inst_path = parts[1].strip()
-						try:
-							with open(inst_path, 'r') as input_file:
-								for file_line in input_file:
-									if ("ERROR" in file_line):
-										writer.writerow([inst_name, file_line.strip(), inst_path])
-						except Exception as e:
-							print(f"Issue with writing in csv file: {e}")	
-	except Exception as ie:
-		print(f"Issue with reading a log file: {ie}")
+class RecordingInHtmlStrategy(RecordingStrategy):
 
+	def record(self, qa_check_path):
+		data = defaultdict(list)
+		output_html = "errors_report.html"
 
-
-
-
-def writeIntoHtml():
-	try:
-		with open(txt_file, 'r') as log_file:
-			for line in log_file:
-				parts = line.strip().split(":")
-				if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
-					print("Failed instance is {")
-					if(len(parts) >= 2):
-						inst_name = parts[0].strip()
-						inst_name = os.path.splitext(inst_name)[0]
-						inst_path = parts[1].strip()
-						try:
-							with open(inst_path, 'r') as input_file:
-								for file_line in input_file:
-									if ("ERROR" in file_line):
-										error_message = file_line.strip().replace("<", "&lt;").replace(">", "&gt;")
-										#html.append(f"<tr><td>{inst_name }</td><td>{error_message}</td><td>{inst_path}</td></tr>")
-										data[inst_name].append((error_message, inst_path))
-						except Exception as e:
-							print(f"Issue with writing in csv file: {e}")	
-	except Exception as ie:
-		print(f"Issue with reading a log file: {ie}")
+		html = [
+    		"<html><head><style>",
+    		"table { border-collapse: collapse; width: 100%; }",
+    		"th, td { border: 1px solid #ccc; padding: 8px; text-align: left; }",
+    		"th { background-color: #f2f2f2; }",
+    		"</style></head><body>",
+    		"<h2>Hello Messages</h2>",
+    		"<table>",
+    		"<tr><th>Instance name</th><th>Error message</th><th>Log Path</th></tr>"
+			]
+			
+		try:
+			with open(txt_file, 'r') as log_file:
+				for line in log_file:
+					parts = line.strip().split(":")
+					if re.search(r"\bErrors:\s*(?!0\b)\d+\b", line):
+						if(len(parts) >= 2):
+							inst_name = parts[0].strip()
+							inst_name = os.path.splitext(inst_name)[0]
+							inst_path = parts[1].strip()
+							try:
+								with open(inst_path, 'r') as input_file:
+									for file_line in input_file:
+										if ("ERROR" in file_line):
+											error_message = file_line.strip().replace("<", "&lt;").replace(">", "&gt;")
+											#html.append(f"<tr><td>{inst_name }</td><td>{error_message}</td><td>{inst_path}</td></tr>")
+											data[inst_name].append((error_message, inst_path))
+							except Exception as e:
+								print(f"Issue with writing in csv file: {e}")	
+		except Exception as ie:
+			print(f"Issue with reading a log file: {ie}")
 		
 		
-		
-	for inst_name, inst_name_values in data.items():
-		inst_name_values = [i for i in inst_name_values if i[0].strip() and i[1].strip()]
-		if (not inst_name_values):
-			continue
+		#this part is only responsible for html formatting???????????????????
+		for inst_name, inst_name_values in data.items():
+			inst_name_values = [i for i in inst_name_values if i[0].strip() and i[1].strip()]
+			if (not inst_name_values):
+				continue
 				
-		rowspan = len(inst_name_values)
-		for j, (error_message, path) in enumerate(inst_name_values):
-			if j == 0:
-				html.append(f"<tr><td rowspan='{rowspan}'>{inst_name}</td><td>{error_message}</td><td>{inst_path}</td></tr>")
-			else:
-				html.append(f"<tr><td>{error_message}</td><td>{inst_path}</td></tr>")
+			rowspan = len(inst_name_values)
+			for j, (error_message, path) in enumerate(inst_name_values):
+				if j == 0:
+					html.append(f"<tr><td rowspan='{rowspan}'>{inst_name}</td><td>{error_message}</td><td>{inst_path}</td></tr>")
+				else:
+					html.append(f"<tr><td>{error_message}</td><td>{inst_path}</td></tr>")
 				
 						
-	html.extend(["</table>", "</body></html>"])
+		html.extend(["</table>", "</body></html>"])
 	
-	with open(output_html, 'w') as out_html:
-		out_html.write("\n".join(html))
+		with open(output_html, 'w') as out_html:
+			out_html.write("\n".join(html))
 
 
 	    
-	    
+	  
+class Recorder:
+	 		
+	def __init__(self, strategy):
+		"""
+		Initialize with a recording strategy
+
+		Args:
+			strategy (RecordingStrategy instance): New recording strategy
+		"""
+
+		self.strategy = strategy
+			
+		
+	def findInstances(self):	
+		try:
+			with open(txt_file, 'w') as txt:
+				for d in directories:
+					if os.path.isdir(d):
+						print("for testing ", d)
+						for file_name in os.listdir(d):
+							full_path = os.path.join(d, file_name)
+							if os.path.isfile(full_path) and file_name.endswith(".log"):
+								try:
+									with open(full_path, 'r') as log_file:
+										for line in log_file:
+											if "Errors:" in line:
+												ls = line.strip()
+												print(ls)
+												instance = os.path.basename(full_path)
+												txt.write(f"{instance} : {full_path} : {line.strip()}\n")
+								except Exception as re:
+									print(f"Issue with reading a file: {re}")
+							elif os.path.isfile(d):
+								print(f"{d} is file")
+		except Exception as we:
+			print(f"Issue with writing into a file: {we}")	  
+			
+			
+			
+	def record_data(self, qa_check_path):
+		return self.strategy.record(qa_check_path)
     
-findInstances()
-writeIntoCsv()
-writeIntoHtml()
 
+
+
+#please provide path for QA check, currently it is empty
+qa_check_path = ""
+
+txt_file = "errors.txt"
+directories = makeDirectoryPath(qa_check_path)
+
+
+#create recorder with csv recording strategy	    
+csv_strategy = RecordingInCsvStrategy()
+csv_recorder = Recorder(csv_strategy)
+
+#record data using the current strategy (Csv)
+csv_recorded_data = csv_recorder.record_data(directories)
 	    
-	    
-	   
+
+#create recorder with html recording strategy	    
+html_strategy = RecordingInHtmlStrategy()
+html_recorder = Recorder(html_strategy)
+
+#record data using the current strategy (Html)
+html_recorded_data = html_recorder.record_data(directories)
 	   
  
 
